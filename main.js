a2cf832eced2f8a038e15c171752015327c0ecfa
NiklasRulec <niklasrulec.98@gmail.com>
2023-04-27 14:56:23 +0200

    upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function changeBackground() {
+  let value1 = Number(document.getElementById("red").value);
+  let value2 = Number(document.getElementById("green").value);
+  let value3 = Number(document.getElementById("blue").value);
+  console.log(value1, value2, value3);
+  let color = "rgb(" + value1 + "," + value2 + "," + value3 + ")";
+  console.log(color);
+  document.getElementById("background").style.backgroundColor = color;
+}
